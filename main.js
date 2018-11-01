7b361aa9720104fa62fc927fb0a974db1a3aa5d6
wuzhiming <wuzhiming.chukong@gmail.com>
2018-11-01 11:40:22 +0800

    add event record

--- main.js
+++ main.js
@@ -81,6 +81,15 @@ async function _handleIOS(options) {
     }
 }
 
+function trackEvent() {
+    let config = Editor._projectProfile.data['facebook'];
+    Editor.Metrics.trackEvent({
+        category: 'Facebook',
+        action: 'Facebook Audience Network APPID',
+        label: config.appID || "",
+    });
+}
+
 async function handleEvent(options, cb) {
     let config = Editor._projectProfile.data['facebook'];
 
@@ -88,6 +97,8 @@ async function handleEvent(options, cb) {
         cb && cb();
         return;
     }
+
+    trackEvent();
 
     if (options.actualPlatform.toLowerCase() === 'android') {
         await _handleAndroid(options).catch((e) => {
